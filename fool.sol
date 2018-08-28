a7a51f6663696d984b5cf1c46ff902fce94db3f8
Ewout Boelsums <ewout.boelsums@gmail.com>
2018-08-28 08:56:10 +0200

    Code refactorting

--- fool.sol
+++ fool.sol
@@ -1,15 +1,15 @@
 pragma solidity 0.4.24;
 
+import './Nostradamus.sol';
+
 contract Fool {
     
-    function sendToNostradamus(address watch_addr) public returns(bytes32 exact, address sender, bool success) {
+    function sendToNostradamus(address watch_addr) public {
     
         uint blockNumber = block.number;
         bytes32 blockHash = blockhash(blockNumber);
-        exact = keccak256(abi.encodePacked(address(this), blockNumber, blockHash, block.timestamp, watch_addr));
-        success = watch_addr.call(bytes4(keccak256("prophecise(bytes32)")), exact);
-        return (exact, msg.sender, success);
+        bytes32 exact = keccak256(abi.encodePacked(address(this), blockNumber, blockHash, block.timestamp, watch_addr));
+        Nostradamus nostradamus = Nostradamus(watch_addr);
+        return nostradamus.prophecise(exact);
         }
-        
-    function () public payable {}
 }
